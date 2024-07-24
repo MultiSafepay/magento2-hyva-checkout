a79eaaed33269f430f63540d539ab5d4ad32dd84
Vinod Sowdagar <vinod.sowdagar@multisafepay.com>
2024-07-24 14:49:37 +0200

    PLGMAG2V2-776: Add payment component to (BNPL) supported methods

--- Payment/Method/MultiSafepayPaymentComponent.php
+++ Payment/Method/MultiSafepayPaymentComponent.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is provided with Magento in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- *
- * Copyright © MultiSafepay, Inc. All rights reserved.
- * See DISCLAIMER.md for disclaimer details.
- */
 
 declare(strict_types=1);
 
@@ -27,9 +16,10 @@ use MultiSafepay\ConnectCore\Logger\Logger;
 use MultiSafepay\ConnectCore\Util\ApiTokenUtil;
 use MultiSafepay\ConnectCore\Util\JsonHandler;
 use MultiSafepay\ConnectCore\Util\RecurringTokensUtil;
+use MultiSafepay\Exception\InvalidDataInitializationException;
 use Rakit\Validation\Validator;
 
-class MultiSafepayCreditCard extends Component\Form
+class MultiSafepayPaymentComponent extends Component\Form
 {
     /**
      * @var ApiTokenUtil
@@ -112,6 +102,11 @@ class MultiSafepayCreditCard extends Component\Form
     public string $country = '';
 
     /**
+     * @var string $methodCode
+     */
+    public string $methodCode = '';
+
+    /**
      * @var string|null
      */
     public ?string $tokens = null;
@@ -156,6 +151,9 @@ class MultiSafepayCreditCard extends Component\Form
 
     /**
      * @return void
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     * @throws InvalidDataInitializationException
      */
     public function mount(): void
     {
@@ -167,6 +165,7 @@ class MultiSafepayCreditCard extends Component\Form
         $this->locale = $this->localeResolver->getLocale();
         $this->country = $this->getQuote()->getBillingAddress()->getCountryId() ?? '';
         $this->tokens = $this->getTokens();
+        $this->methodCode = $this->getQuote()->getPayment()->getMethod() ?? '';
     }
 
     /**
@@ -189,6 +188,8 @@ class MultiSafepayCreditCard extends Component\Form
      * Get the gateway code
      *
      * @return string
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
     private function getGatewayCode(): string
     {
@@ -205,6 +206,8 @@ class MultiSafepayCreditCard extends Component\Form
      * Get the recurring tokens if they exist
      *
      * @return string|null
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
     private function getTokens(): ?string
     {
@@ -239,6 +242,8 @@ class MultiSafepayCreditCard extends Component\Form
      * Check if payment component is enabled
      *
      * @return bool
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
     public function isPaymentComponentEnabled(): bool
     {
@@ -259,7 +264,8 @@ class MultiSafepayCreditCard extends Component\Form
      * @param array $paymentComponentData
      * @return void
      */
-    public function setPaymentComponentData(array $paymentComponentData) {
+    public function setPaymentComponentData(array $paymentComponentData)
+    {
         $additionalInformation = [];
 
         if (isset($paymentComponentData['tokenize']) && $paymentComponentData['tokenize']) {
