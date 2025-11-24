f07ba1641db9d09b4c265d8c263d68b780caf07a
Vinod Sowdagar <vinod.sowdagar@multisafepay.com>
2025-11-24 12:10:01 +0100

    HYVA-40: Add Vault support for DIRDEB and MISTERCASH

---
+++ Payment/Method/DirectDebitVault.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\HyvaCheckout\Payment\Method;
+
+use MultiSafepay\ConnectCore\Model\Ui\Gateway\DirectDebitRecurringConfigProvider;
+
+class DirectDebitVault extends PaymentVault
+{
+    public const VAULT_CODE = DirectDebitRecurringConfigProvider::CODE;
+}
